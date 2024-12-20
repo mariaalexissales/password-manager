dd176f6812b03bedb31cd4a4782a39907d352c95
Maria Alexis Sales <mariaalexissales@gmail.com>
2024-12-20 01:05:54 -0800

    file already generated edge case

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
 import base64
 import os
+import sys
 import argparse
+from pathlib import Path
+
+# Logic
 from cryptography.fernet import Fernet
+
+# Data
+import sqlite3
+database = sqlite3.connect("passwords.db")
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--init',action='store_true', help="Initializes manager")
-    parser.add_argument('--add', help="Stores password")
+    parser.add_argument('--init', action='store_true', help="Initializes manager")
+    parser.add_argument('--add', nargs=2, metavar=('app', 'password'), action='store', help="Stores password")
     args = parser.parse_args()
     
     if args.init:
+        key_gen = Path("./manager.key")
+        
+        if key_gen.is_file():
+            print("Encryption key is already generated")
+            return
         key = Fernet.generate_key()
-        with open("manager.key", "wb") as key_file:
+        with open(key_gen, "wb") as key_file:
             key_file.write(key)
         
         print("Key generated")
